14ec9fb0fc4b55db5a2fd5a8a49f2dd371451e4e
oedesign <oluwaseyielujoba15@gmail.com>
2025-11-06 12:32:44 +0100

    page layout

--- scripts.js
+++ scripts.js
@@ -10,3 +10,26 @@
         toggleIcon.textContent = '✖'; // Delete (close) icon
       }
     });
+
+    // 
+
+    //  Get current year
+const year = new Date().getFullYear();
+
+// Insert into span
+document.getElementById("currentyear").textContent = year;
+
+// Get the last modified date of the document
+let lastmodification = document.lastModified
+
+// Display it inside the paragraph
+document.getElementById("lastModified").textContent = 
+"This page was last modified on: " + lastmodification;
+
+// Styling the lastModified element
+document.getElementById("lastModified").style.color = "black";
+
+
+const variableName = document.lastModified; // Get the last modified date of the document
+// console.log('This page was last modified on: ' + variableName); // Print the last modified date to the console
+document.getElementById('').textContent = 'Last Modified: ' + variableName; // Optionally set the text of an element with id 'last-modified'
