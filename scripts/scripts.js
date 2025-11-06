95b82c1f809a35a4a3fd8223956f81b3ea203c81
oedesign <oluwaseyielujoba15@gmail.com>
2025-11-06 13:28:15 +0100

    js adjustment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,22 @@
- const toggleIcon = document.getElementById('toggleIcon');
-    const menu = document.querySelector('.nav-links');
+const toggleIcon = document.getElementById('toggleIcon');
+const menu = document.querySelector('.nav-links');
 
-    toggleIcon.addEventListener('click', () => {
-      if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-        toggleIcon.textContent = '☰'; // Menu bar icon
-      } else {
-        menu.style.display = 'block';
-        toggleIcon.textContent = '✖'; // Delete (close) icon
-      }
-    });
+toggleIcon.addEventListener('click', () => {
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+    toggleIcon.textContent = '☰'; // Menu bar icon
+  } else {
+    menu.style.display = 'block';
+    toggleIcon.textContent = '✖'; // Close icon
+  }
+});
 
-    // 
-
-    //  Get current year
+// Get current year
 const year = new Date().getFullYear();
-
-// Insert into span
 document.getElementById("currentyear").textContent = year;
 
 // Get the last modified date of the document
-let lastmodification = document.lastModified
-
-// Display it inside the paragraph
-document.getElementById("lastModified").textContent = 
-"This page was last modified on: " + lastmodification;
-
-// Styling the lastModified element
+const lastmodification = document.lastModified;
+document.getElementById("lastModified").textContent =
+  "This page was last modified on: " + lastmodification;
 document.getElementById("lastModified").style.color = "black";
-
-
-const variableName = document.lastModified; // Get the last modified date of the document
-// console.log('This page was last modified on: ' + variableName); // Print the last modified date to the console
-document.getElementById('').textContent = 'Last Modified: ' + variableName; // Optionally set the text of an element with id 'last-modified'
