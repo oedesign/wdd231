be5ef63b95463a7e63798aec6852abc218732035
oedesign <oluwaseyielujoba15@gmail.com>
2025-12-08 16:50:29 +0100

    Feed The Nation

---
+++ final-project/scripts/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const navigation = document.getElementById("navigation");
+
+    hamburger.addEventListener("click", () => {
+        navigation.classList.toggle("show");
+
+    // Toggle hamburger icon to X and back
+    if (navigation.classList.contains("show")) {
+        hamburger.innerHTML = "✖";
+        } else {
+        hamburger.innerHTML = "☰";
+        }
+    });
